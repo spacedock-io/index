c461b0b8243356fd513f744033e96f45c5ee59a8
Colton Baker <github@netrefuge.net>
2013-12-28 21:12:37 -0500

    Route skeleton

---
+++ routes.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "github.com/ricallinson/forgery"
+  "github.com/stretchr/objx"
+)
+
+func Routes(config objx.Map, server *f.Server) {
+  /*
+    Library repository routes
+   */
+  server.Put("/v1/repositories/:repo/auth", LibraryAuth)
+  server.Put("/v1/repositories/:repo", CreateLibrary)
+  server.Delete("/v1/repositories/:repo", DeleteLibrary)
+  server.Put("/v1/repositories/:repo/images", UpdateLibraryImage)
+  server.Get("/v1/repositories/:repo/images", GetLibraryImage)
+
+  /*
+    User routes
+   */
+  server.Get("/v1/users", Login)
+  server.Post("/v1/users", CreateUser)
+  server.Put("/v1/users", UpdateUser)
+
+  /*
+    User repository routes
+   */
+  server.Put("/v1/repositories/:namespace/:repo/auth", RepoAuth)
+  server.Put("/v1/repositories/:namespace/:repo", CreateRepo)
+  server.Delete("/v1/repositories/:namespace/:repo", DeleteRepo)
+  server.Put("/v1/repositories/:namespace/:repo/images", UpdateUserImage)
+  server.Get("/v1/repositories/:namespace/:repo/images", GetUserImage)
+
+  // Search route
+  server.Get("/v1/search", Search)
+}
---
+++ user.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "github.com/ricallinson/forgery"
+)
+
+func CreateUser(req *f.Request, res *f.Response, next func()) {
+  res.Send("Not implemented yet.")
+}
+
+func Login(req *f.Request, res *f.Response, next func()) {
+  res.Send("Not implemented yet.")
+}
+
+func UpdateUser(req *f.Request, res *f.Response, next func()) {
+  res.Send("Not implemented yet.")
+}
