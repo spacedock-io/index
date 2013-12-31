8257037655a099f81789f7b53badb0210278a8b6
Maciej Małecki <me@mmalecki.com>
2013-12-31 15:16:14 +0100

    Saner assertions

--- models/user_test.go
+++ models/user_test.go
@@ -2,6 +2,7 @@ package models_test
 
 import (
   "testing"
+  "github.com/stretchr/testify/assert"
   "github.com/yawnt/index.spacedock/config"
   "github.com/yawnt/index.spacedock/couch"
   "github.com/yawnt/index.spacedock/models"
@@ -12,12 +13,14 @@ func init() {
   couch.Couch = couch.New()
 }
 
+func TestUserGet(t *testing.T) {
+  user, err := models.GetUser("mmalecki")
+  assert.Nil(t, err, "Error should be `nil`")
+  assert.NotNil(t, user, "User should not be `nil`")
+}
+
 func TestUserGetNoSuchUser(t *testing.T) {
   user, err := models.GetUser("404")
-  if err != nil {
-    t.Errorf("Error should be `nil`, got: %s", err)
-  }
-  if user != nil {
-    t.Errorf("User should be `nil`, got: %s", user)
-  }
+  assert.Nil(t, err, "Error should be `nil`")
+  assert.Nil(t, user, "User should be `nil`")
 }
