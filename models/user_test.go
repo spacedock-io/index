c1508dd856e012422b328655747becc0c8b5d80e
Colton Baker <github@netrefuge.net>
2014-01-02 11:12:43 -0500

    "models_test" -> "models"

--- models/user_test.go
+++ models/user_test.go
@@ -1,11 +1,10 @@
-package models_test
+package models
 
 import (
   "testing"
   "github.com/stretchr/testify/assert"
   "github.com/spacedock-io/index/config"
   "github.com/spacedock-io/index/couch"
-  "github.com/spacedock-io/index/models"
 )
 
 func init() {
@@ -15,14 +14,14 @@ func init() {
 
 func TestUserGet(t *testing.T) {
   username := "mmalecki"
-  user, err := models.GetUser(username)
+  user, err := GetUser(username)
   assert.Nil(t, err, "Error should be `nil`")
   assert.NotNil(t, user, "User should not be `nil`")
   assert.Equal(t, user.Username, username, "Username should be correct")
 }
 
 func TestUserGetNoSuchUser(t *testing.T) {
-  user, err := models.GetUser("404")
+  user, err := GetUser("404")
   assert.Nil(t, err, "Error should be `nil`")
   assert.Nil(t, user, "User should be `nil`")
 }
@@ -32,32 +31,32 @@ func TestUserCreateAndDestroy(t *testing.T) {
   // destroy user in one test.
   username := "foo"
 
-  user := &models.User{
+  user := &User{
     Username: username,
   }
 
-  err := models.CreateUser(user)
+  err := CreateUser(user)
   assert.Nil(t, err, "Error should be `nil`")
 
-  getUser, getError := models.GetUser(username)
+  getUser, getError := GetUser(username)
   assert.Nil(t, getError, "Get error should be `nil`")
   assert.Equal(t, getUser.Username, username)
 
-  err = models.DeleteUser(username)
+  err = DeleteUser(username)
   assert.Nil(t, err, "Delete error should be `nil`")
 
-  getUser, getError = models.GetUser(username)
+  getUser, getError = GetUser(username)
   assert.Nil(t, getUser, "User shouldn't exists after being deleted")
 }
 
 func TestUserCreateAlreadyExists(t *testing.T) {
   username := "mmalecki"
 
-  user := &models.User{
+  user := &User{
     Username: username,
   }
 
-  err := models.CreateUser(user)
+  err := CreateUser(user)
   assert.NotNil(t, err, "Error should not be `nil`")
-  assert.IsType(t, err, models.AlreadyExistsError{})
+  assert.IsType(t, err, AlreadyExistsError{})
 }
