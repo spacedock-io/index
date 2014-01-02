d75aeac30838792872e5e9a1a073e1bc7aac1f50
Colton Baker <github@netrefuge.net>
2014-01-02 13:10:50 -0500

    Clean up the user test

--- models/user_test.go
+++ models/user_test.go
@@ -7,56 +7,43 @@ import (
   "github.com/spacedock-io/index/couch"
 )
 
+var (
+  username = "foo"
+  user = &User{
+    Username: username,
+  }
+)
+
 func init() {
   config.Global = config.Load("test")
   couch.Couch = couch.New()
 }
 
-func TestUserGet(t *testing.T) {
-  username := "mmalecki"
-  user, err := GetUser(username)
-  assert.Nil(t, err, "Error should be `nil`")
-  assert.NotNil(t, user, "User should not be `nil`")
-  assert.Equal(t, user.Username, username, "Username should be correct")
-}
-
-func TestUserGetNoSuchUser(t *testing.T) {
-  user, err := GetUser("404")
-  assert.Nil(t, err, "Error should be `nil`")
-  assert.Nil(t, user, "User should be `nil`")
-}
-
-func TestUserCreateAndDestroy(t *testing.T) {
+func TestUserCreate(t *testing.T) {
   // Since tests are ran in parallel (in goroutines), we need to create and
   // destroy user in one test.
-  username := "foo"
-
-  user := &User{
-    Username: username,
-  }
-
   err := CreateUser(user)
   assert.Nil(t, err, "Error should be `nil`")
-
-  getUser, getError := GetUser(username)
-  assert.Nil(t, getError, "Get error should be `nil`")
-  assert.Equal(t, getUser.Username, username)
-
-  err = DeleteUser(username)
-  assert.Nil(t, err, "Delete error should be `nil`")
-
-  getUser, getError = GetUser(username)
-  assert.Nil(t, getUser, "User shouldn't exists after being deleted")
 }
 
-func TestUserCreateAlreadyExists(t *testing.T) {
-  username := "mmalecki"
-
-  user := &User{
-    Username: username,
-  }
-
+func TestAlreadyExists(t *testing.T) {
   err := CreateUser(user)
   assert.NotNil(t, err, "Error should not be `nil`")
   assert.IsType(t, err, AlreadyExistsError{})
+}
+
+func TestUserGet(t *testing.T) {
+  getUser, getError := GetUser(username)
+  assert.Nil(t, getError, "Get error should be `nil`")
+  assert.Equal(t, getUser.Username, username)
+}
+
+func TestUserDelete(t *testing.T) {
+  err := DeleteUser(username)
+  assert.Nil(t, err, "Delete error should be `nil`")  
+}
+
+func TestNoSuchUser(t *testing.T) {
+  getUser, _ := GetUser(username)
+  assert.Nil(t, getUser, "User shouldn't exists after being deleted")
 }
