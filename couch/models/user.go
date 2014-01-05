f471257f8c5cccf75f8b3a20b388a81ce30c1bc6
Charlie McConnell <charlie@charlieistheman.com>
2014-01-05 13:55:25 -0500

    Add user emails and pbkdf2 password hashing

--- couch/models/user.go
+++ couch/models/user.go
@@ -1,15 +1,24 @@
 package models
 
 import (
+  "crypto/sha256"
   "github.com/spacedock-io/index/couch"
   "github.com/fjl/go-couchdb"
+  "github.com/gokyle/pbkdf2"
 )
 
 var prefix string = "user:"
 
 type User struct {
   Username string `json:"username"`
+  Email string `json:"email"`
+  Salt string `json:"salt"`
+  Pass string `json:"pass"`
   Rev string `json:"_rev,omitempty"`
+}
+
+func init() {
+  pbkdf2.HashFunc = sha256.New
 }
 
 func NewUser() *User {
@@ -30,7 +39,12 @@ func GetUser(name string) (*User, error) {
   return ret, err
 }
 
-func CreateUser(user *User) error {
+func CreateUser(user *User, password string) error {
+  ph := pbkdf2.HashPassword(password)
+
+  user.Salt = string(ph.Salt)
+  user.Pass = string(ph.Hash)
+
   _, err := couch.Global.Put(prefix + user.Username, user)
   if err != nil {
     dberr, ok := err.(couchdb.DatabaseError)
@@ -51,3 +65,11 @@ func DeleteUser(name string) error {
   _, err = couch.Global.Delete(prefix + user.Username, user.Rev)
   return err
 }
+
+func AuthUser(name string, pass string) bool {
+  ret, err := GetUser(name)
+  if err != nil { return false }
+  ph := &pbkdf2.PasswordHash{[]byte(ret.Pass), []byte(ret.Salt)}
+  return pbkdf2.MatchPassword(pass, ph)
+}
+
