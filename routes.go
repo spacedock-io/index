cba784c7d1268978066e3e9049f1d9513e220182
Colton Baker <github@netrefuge.net>
2014-01-08 15:21:24 -0500

    Add the trailing slash to the rest of the routes.
    The router in forgery doesn't add the trailing slash itself if it's not
    already found. Instead, it makes the last trailing slash optional unless
    forgery.Strict is true.

--- routes.go
+++ routes.go
@@ -9,11 +9,11 @@ func Routes(server *f.Server) {
   /*
     Library repository routes
    */
-  server.Put("/v1/repositories/:repo/auth", LibraryAuth)
-  server.Put("/v1/repositories/:repo", CreateLibrary)
-  server.Delete("/v1/repositories/:repo", DeleteLibrary)
-  server.Put("/v1/repositories/:repo/images", UpdateLibraryImage)
-  server.Get("/v1/repositories/:repo/images", GetLibraryImage)
+  server.Put("/v1/repositories/:repo/auth/", LibraryAuth)
+  server.Put("/v1/repositories/:repo/", CreateLibrary)
+  server.Delete("/v1/repositories/:repo/", DeleteLibrary)
+  server.Put("/v1/repositories/:repo/images/", UpdateLibraryImage)
+  server.Get("/v1/repositories/:repo/images/", GetLibraryImage)
 
   /*
     User routes
@@ -25,12 +25,12 @@ func Routes(server *f.Server) {
   /*
     User repository routes
    */
-  server.Put("/v1/repositories/:namespace/:repo/auth", RepoAuth)
-  server.Put("/v1/repositories/:namespace/:repo", CreateRepo)
-  server.Delete("/v1/repositories/:namespace/:repo", DeleteRepo)
-  server.Put("/v1/repositories/:namespace/:repo/images", UpdateUserImage)
-  server.Get("/v1/repositories/:namespace/:repo/images", GetUserImage)
+  server.Put("/v1/repositories/:namespace/:repo/auth/", RepoAuth)
+  server.Put("/v1/repositories/:namespace/:repo/", CreateRepo)
+  server.Delete("/v1/repositories/:namespace/:repo/", DeleteRepo)
+  server.Put("/v1/repositories/:namespace/:repo/images/", UpdateUserImage)
+  server.Get("/v1/repositories/:namespace/:repo/images/", GetUserImage)
 
   // Search route
-  server.Get("/v1/search", Search)
+  server.Get("/v1/search/", Search)
 }
