0905a9abe4f48bdb0998310e49667bfdbf903d1a
Colton Baker <github@netrefuge.net>
2014-01-10 18:35:39 -0500

    req.Map -> req.Map["json"]

--- user.go
+++ user.go
@@ -12,9 +12,10 @@ func CreateUser(req *f.Request, res *f.Response, next func()) {
     username, email, password = req.Body["username"], req.Body["email"],
       req.Body["password"]
   } else if len(req.Request.Map) > 0 {
-    username, _ = req.Map["username"].(string)
-    password, _ = req.Map["password"].(string)
-    email, _ = req.Map["email"].(string)
+    json := req.Map["json"].(map[string]string)
+    username, _ = json["username"]
+    password, _ = json["password"]
+    email, _ = json["email"]
   }
 
   // @TODO: Validate email format
