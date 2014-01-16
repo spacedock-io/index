81a3e9bf321be9c40ff16cfc150323667fe5608a
Charlie McConnell <charlie@charlieistheman.com>
2014-01-15 22:19:59 -0500

    Remove unneeded fields from registry

--- models/repo.go
+++ models/repo.go
@@ -17,9 +17,7 @@ type Repo struct {
 type Image struct {
   Id            int64   `json:"-"`
   Uuid          string  `json:"id"`
-  Json          []byte  `json:"-"`
   Checksum      string  `json:"checksum,omitempty"`
-  Size          int64   `json:"size,omitempty"`
   RepoId        int64   `json:"-"`
 }
 
