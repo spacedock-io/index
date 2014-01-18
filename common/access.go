a91b795e5ec4f3ad12f6c534dd3e44e8c3e7d2fd
Colton Baker <github@netrefuge.net>
2014-01-17 19:44:42 -0500

    Merge all of the access middleware into common.Access

--- common/access.go
+++ common/access.go
@@ -13,6 +13,33 @@ var AccessMap = map[string]int{
   "delete": 3,
 }
 
+func sendToken(req *f.Request, res *f.Response, access string) {
+  user := req.Map["_user"].(*models.User)
+  ns := req.Params["namespace"]
+  repo := req.Params["repo"]
+
+  ok := hasAccess(user, ns, repo, access)
+  if !ok {
+    res.Send("You do not have access to perform this action.", 400)
+  }
+
+  if len(ns) == 0 {
+    ns = "library"
+  }
+
+  repo = ns + "/" + repo
+
+  if wantsToken(req) {
+    token, err := models.GetToken(user, repo, access)
+    if err != nil {
+      res.Send(err.Error(), 400)
+      return
+    }
+    res.Set("x-docker-token", token.String())
+    res.Set("www-authenticate", "Token " + token.String())
+  }
+}
+
 func wantsToken(req *f.Request) bool {
   return strings.ToLower(strings.Trim(req.Get("x-docker-token"), " ")) == "true"
 }
@@ -21,84 +48,8 @@ func hasAccess(user *models.User, ns, repo, access string) bool {
   return AccessMap[access] <= AccessMap[user.GetAccess(ns, repo)]
 }
 
-func DeleteAccess(req *f.Request, res *f.Response, next func()) {
-  user := req.Map["_user"].(*models.User)
-  ns := req.Params["namespace"]
-  repo := req.Params["repo"]
-
-  ok := hasAccess(user, ns, repo, "delete")
-  if !ok {
-    res.Send("You do not have access to delete this repository.", 400)
-  }
-
-  if len(ns) == 0 {
-    ns = "library"
-  }
-
-  repo = ns + "/" + repo
-
-  if wantsToken(req) {
-    token, err := models.GetToken(user, repo, "delete")
-    if err != nil {
-      res.Send(err.Error(), 400)
-      return
-    }
-    res.Set("x-docker-token", token.String())
-    res.Set("www-authenticate", "Token " + token.String())
-  }
-}
-
-func ReadAccess(req *f.Request, res *f.Response, next func()) {
-  user := req.Map["_user"].(*models.User)
-  ns := req.Params["namespace"]
-  repo := req.Params["repo"]
-
-  ok := hasAccess(user, ns, repo, "read")
-  if !ok {
-    res.Send("You do not have access to read from this repository.", 400)
-  }
-
-  if len(ns) == 0 {
-    ns = "library"
-  }
-
-  repo = ns + "/" + repo
-
-  if wantsToken(req) {
-    token, err := models.GetToken(user, repo, "read")
-    if err != nil {
-      res.Send(err.Error(), 400)
-      return
-    }
-    res.Set("x-docker-token", token.String())
-    res.Set("www-authenticate", "Token " + token.String())
-  }
-}
-
-func WriteAccess(req *f.Request, res *f.Response, next func()) {
-  user := req.Map["_user"].(*models.User)
-  ns := req.Params["namespace"]
-  repo := req.Params["repo"]
-
-  ok := hasAccess(user, ns, repo, "write")
-  if !ok {
-    res.Send("You do not have access to write to this repository.", 400)
-    return
-  }
-
-  if len(ns) == 0 {
-    ns = "library"
-  }
-
-  repo = ns + "/" + repo
-
-  if wantsToken(req) {
-    token, err := models.GetToken(user, repo, "write")
-    if err != nil {
-      res.Send(err.Error(), 400)
-      return
-    }
-    res.Set("x-docker-token", token.String())
-    res.Set("www-authenticate", "Token " + token.String())
+func Access(access string) func(*f.Request, *f.Response, func()) {
+  return func(req *f.Request, res *f.Response, next func()) {
+    sendToken(req, res, access)
   }
 }
